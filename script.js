469d6c20a24040d449336d17dd4bb7429c130985
SilverioSimoes <silverio.simoes@gmail.com>
2021-10-02 10:07:00 -0300

    Controle de Despesas ok

--- script.js
+++ script.js
@@ -13,46 +13,52 @@ const localStorageTransactions = JSON.parse(
 )
 let transactions =
   localStorage.getItem('transactions') !== null ? localStorageTransactions : []
-
 const removeTransaction = ID => {
   transactions = transactions.filter(transaction => transaction.id !== ID)
   updateLocalStorage()
   init()
 }
 
-const addTransactionIntoDom = transaction => {
-  const operator = transaction.amount < 0 ? '-' : '+'
-  const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
-  const amountWithoutOperator = Math.abs(transaction.amount)
+const addTransactionIntoDom = ({ amount, name, id }) => {
+  const operator = amount < 0 ? '-' : '+'
+  const CSSClass = amount < 0 ? 'minus' : 'plus'
+  const amountWithoutOperator = Math.abs(amount)
   const li = document.createElement('li')
 
   li.classList.add(CSSClass)
   li.innerHTML = `
-  ${transaction.name} <span>${operator} ${real(amountWithoutOperator)}</span>
-  <button class="delete-btn" onclick = "removeTransaction(${
-    transaction.id
-  })">x</button>
+  ${name} <span>${operator} ${real(amountWithoutOperator)}</span>
+  <button class="delete-btn" onClick = "removeTransaction(${id})">x</button>
   `
   transactionUl.append(li)
 }
 
-const updateBalanceValues = () => {
-  const transactionsAmounts = transactions.map(({ amount }) => amount)
-  const total = real(transactionsAmounts.reduce((acc, valor) => acc + valor, 0))
-
-  const income = real(
-    transactionsAmounts
-      .filter(transaction => transaction > 0)
-      .reduce((acc, valor) => acc + valor, 0)
-  )
-
-  const expense = real(
+const getExpenses = transactionsAmounts =>
+  real(
     Math.abs(
       transactionsAmounts
         .filter(transaction => transaction < 0)
         .reduce((acc, valor) => acc + valor, 0)
     )
   )
+
+const getIncome = transactionsAmounts =>
+  real(
+    transactionsAmounts
+      .filter(transaction => transaction > 0)
+      .reduce((acc, valor) => acc + valor, 0)
+  )
+
+const getTotal = transactionsAmounts =>
+  real(transactionsAmounts.reduce((acc, valor) => acc + valor, 0))
+
+const updateBalanceValues = () => {
+  const transactionsAmounts = transactions.map(({ amount }) => amount)
+  const total = getTotal(transactionsAmounts)
+
+  const income = getIncome(transactionsAmounts)
+
+  const expense = getExpenses(transactionsAmounts)
 
   balanceDisplay.textContent = `${total}`
   incomeDisplay.textContent = `${income}`
@@ -73,28 +79,36 @@ const updateLocalStorage = () => {
 
 const generateID = () => Math.round(Math.random() * 1000)
 
-const handleFormSubmit = event => {
-  event.preventDefault()
-
-  const transactionName = inputTransactionName.value.trim()
-  const transactionAmount = inputTransactionAmount.value.trim()
-  if (transactionName === '' || transactionAmount === '') {
-    alert('Por Favor, preencha tanto o nome quanto o valor da transação!')
-    return
-  }
-
+const addTransactionsArray = (transactionName, transactionAmount) => {
   const transaction = {
-    id: generateID,
+    id: generateID(),
     name: transactionName,
     amount: Number(transactionAmount)
   }
 
   transactions.push(transaction)
-  updateLocalStorage()
-  init()
+}
 
+const cleanInputs = () => {
   inputTransactionName.value = ''
   inputTransactionAmount.value = ''
+}
+
+const handleFormSubmit = event => {
+  event.preventDefault()
+
+  const transactionName = inputTransactionName.value.trim()
+  const transactionAmount = inputTransactionAmount.value.trim()
+  const isSomeInputEmpty = transactionName === '' || transactionAmount === ''
+  if (isSomeInputEmpty) {
+    alert('Por Favor, preencha tanto o nome quanto o valor da transação!')
+    return
+  }
+
+  addTransactionsArray(transactionName, transactionAmount)
+  init()
+  updateLocalStorage()
+  cleanInputs()
 }
 
 form.addEventListener('submit', handleFormSubmit)
